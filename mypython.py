6fb6754adae1f73aa544d6c26e43cbd9ea763cad
mani_ee231 <gopalakrishnan.manikandan@gmail.com>
2020-07-02 01:44:30 -0400

    Jenkin status using python and jenkin API

--- mypython.py
+++ mypython.py
@@ -1,1 +1,48 @@
-print('hello world')
+#!/usr/bin/python
+
+import json
+import sys
+import urllib2
+import base64
+
+user = admin
+password = admin
+jenkinsUrl = http://192.168.0.35:8080
+
+
+def urlopen(url, data=None):
+    '''Open a URL using the urllib2 opener.'''
+    request = urllib2.Request(url, data)
+    base64string = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
+    request.add_header("Authorization", "Basic %s" % base64string)
+    response = urllib2.urlopen(request)
+    return response
+
+
+
+if len( sys.argv ) > 1 :
+    jobName = sys.argv[1]
+else :
+    sys.exit(1)
+
+try:
+    jenkinsStream   = urlopen(jenkinsUrl + jobName + "/lastBuild/api/json")
+except urllib2.HTTPError, e:
+    print "URL Error: " + str(e.code)
+    print "      (job name [" + jobName + "] probably wrong)"
+    sys.exit(2)
+
+try:
+    buildStatusJson = json.load( jenkinsStream )
+except:
+    print "Failed to parse json"
+    sys.exit(3)
+
+if buildStatusJson.has_key( "result" ):
+    print "[" + jobName + " #" + str(buildStatusJson["number"]) + "]: " + buildStatusJson["result"]
+    if buildStatusJson["result"] != "SUCCESS" :
+		exit(4)
+else:
+	sys.exit(5)
+
+sys.exit(0)
